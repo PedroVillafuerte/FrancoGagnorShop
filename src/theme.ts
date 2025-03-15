3aee26c89b17f62665f410c0c1d297b7ae78a84f
Luxx <lq.santos@hotmail.com>
2025-03-14 21:22:52 -0300

    font on theme

--- src/theme.ts
+++ src/theme.ts
@@ -1,5 +1,5 @@
 import { createTheme, responsiveFontSizes } from '@mui/material/styles'
-import testfont from '../src/assets/fonts/BigShouldersInline-VariableFont_opsz,wght.ttf'
+import quicksandFont from '../src/assets/fonts/Quicksand/Quicksand-VariableFont_wght.ttf'
 
 let theme = createTheme({
   palette: {
@@ -20,18 +20,18 @@ let theme = createTheme({
     },
   },
   typography: {
-    fontFamily: 'Big Shoulders Inline',
+    fontFamily: 'Quicksand',
   },
   components: {
     MuiCssBaseline: {
       styleOverrides: `
         @font-face {
-          font-family: "Big Shoulders Inline";
+          font-family: "Quicksand";
           font-optical-sizing: auto;
           font-style: normal;
           font-display: swap;
-          font-weight: 400;
-          src: local('Raleway'), local('Raleway-Regular'), url(${testfont}) format('woff2');
+          font-weight: 200;
+          src: local('Raleway'), local('Raleway-Regular'), url(${quicksandFont}) format('woff2');
           unicodeRange: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF;
         }
       `,
